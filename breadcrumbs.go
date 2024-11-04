931e720ce3f10fd2e335f45e07af156395e81a21
Pavel <pavel@p-p.name>
2024-11-04 17:52:20 -0500

    * small fix breadcrumbs.go

--- breadcrumbs.go
+++ breadcrumbs.go
@@ -6,7 +6,7 @@ func (c *Impl) breadcrumbBuilder(config *modelConfig) string {
 	breadcrumbStr += `    <li class="breadcrumb-item"><a href="/">Home</a></li>` + "\n"
 
 	if parentModelName, parentExists := config.Parent["modelName"]; parentExists {
-		breadcrumbStr += `    <li class="breadcrumb-item"><a href="/render_table/` + parentModelName + `">` + config.ParentConfig.PageTitle + `</a></li>` + "\n"
+		breadcrumbStr += `    <li class="breadcrumb-item"><a href="/wedyta/` + parentModelName + `">` + config.ParentConfig.PageTitle + `</a></li>` + "\n"
 	}
 
 	breadcrumbStr += `    <li class="breadcrumb-item active" aria-current="page">` + config.PageTitle + ` &nbsp; <i class="bi-arrow-repeat" style="color: grey; cursor: pointer;" onClick="window.location.href = window.location.pathname + window.location.search + window.location.hash;"></i>` + `</li>` + "\n"
