b02089883b5c2b8de4a541680cfe80d00f184968
Pavel <pavel@p-p.name>
2024-12-07 22:48:00 -0500

    + resolveVariables()

--- load_config.go
+++ load_config.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -33,7 +34,7 @@ func (c *Impl) loadModelConfig(context *gin.Context, modelName string, payload m
 		//fmt.Println(parentModelName)
 		config.ParentConfig, err = c.loadModelConfig(context, parentModelName, payload)
 		if err != nil {
-			log.Print("Can`t load ParentConfig: " + parentModelName)
+			log.Print("Wedyta: Can`t load ParentConfig: " + parentModelName)
 		}
 
 		if queryVariableName, exist := config.Parent["queryVariableName"]; exist {
@@ -53,14 +54,47 @@ func (c *Impl) loadModelConfig(context *gin.Context, modelName string, payload m
 		}
 	}
 
-	queryParams := context.Request.URL.Query()
-	for key, values := range queryParams {
-		for _, val := range values {
-			//		fmt.Printf("Parameter: %s, Value: %s\n", key, value)
-			placeholder := fmt.Sprintf("$%s$", key)
-			config.SqlWhere = strings.ReplaceAll(config.SqlWhere, placeholder, fmt.Sprintf("%v", val))
+	if c.Config.VariableResolver == nil && strings.Contains(config.SqlWhere, "{{") {
+		return nil, fmt.Errorf("trying to use variables without Config.VariableResolver modelName=%s", modelName)
+	}
+
+	config.SqlWhere = c.resolveVariables(context, modelName, config.SqlWhere)
+
+	return &config, nil
+}
+
+func (c *Impl) resolveVariables(context *gin.Context, modelName string, str string) string {
+	if !strings.Contains(str, "{{") {
+		return str
+	}
+
+	re := regexp.MustCompile(`{{(.*?)}}`)
+	matches := re.FindAllStringSubmatch(str, -1)
+
+	var variables []string
+	for _, match := range matches {
+		if len(match) > 1 {
+			variables = append(variables, match[1])
 		}
 	}
 
-	return &config, nil
+	//queryParams := context.Request.URL.Query()
+	for _, variable := range variables {
+		value := ""
+		//if queryValue, exists := queryParams[variable]; exists {
+		//	value = queryValue[0]
+		//} else {
+		if c.Config.VariableResolver != nil {
+			value = c.Config.VariableResolver(context, modelName, variable)
+		} else {
+
+		}
+
+		if value != "" {
+			str = strings.ReplaceAll(str, "{{"+variable+"}}", fmt.Sprintf("%v", value))
+		} else {
+			log.Printf("Wedyta: Can`t resolve variable='%s' for modelName=%s", variable, modelName)
+		}
+	}
+	return str
 }
