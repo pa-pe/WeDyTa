c961c41a96b1c17c67e58a6a8dc3429b76bb1b92
Pavel <pavel@p-p.name>
2025-06-21 23:14:33 -0400

    + c.renderRecordValue()

--- render_table.go
+++ render_table.go
@@ -122,7 +122,9 @@ func (c *Impl) RenderModelTable(context *gin.Context, db *gorm.DB, modelName str
 	}
 	htmlTable.WriteString("</tr>\n</thead>\n<tbody>\n")
 
-	relatedDataCache := make(map[string]string)
+	//relatedDataCache := make(map[string]string)
+	var cache RenderTableCache
+	cache.RelatedData = make(map[string]string)
 
 	for _, record := range records {
 		trClass := ""
@@ -134,104 +136,7 @@ func (c *Impl) RenderModelTable(context *gin.Context, db *gorm.DB, modelName str
 
 		htmlTable.WriteString("<tr" + trClass + ">\n")
 		for _, field := range config.Fields {
-			//value := record[field]
-			value, exists := record[field]
-			if !exists || value == nil {
-				value, exists = record[InvertCaseStyle(field)]
-				if !exists || value == nil {
-					value = ""
-				}
-			}
-
-			fldCfg := config.FieldConfig[field]
-
-			classStr := ""
-			additionalAttr := ""
-			if field == "id" || field == "ID" {
-				classStr += " rec_id"
-			}
-
-			if fldCfg.Classes != "" {
-				classStr += " " + fldCfg.Classes
-			}
-
-			if fldCfg.IsEditable {
-				classStr += " editable editable-" + fldCfg.FieldEditor
-				additionalAttr += ` fieldName="` + CamelToSnake(field) + `"`
-			}
-
-			classAttr := ""
-			if classStr != "" {
-				classStr = classStr[1:]
-				classAttr = fmt.Sprintf(" class='%s'", classStr)
-			}
-			tagAttrs := classAttr + additionalAttr
-
-			relatedDataField, relatedExists := config.RelatedData[field]
-			if relatedExists {
-				//relatedDataField = utils.CamelToSnake(relatedDataField)
-				cacheKey := fmt.Sprintf("%s_%v", relatedDataField, value)
-				if cachedValue, found := relatedDataCache[cacheKey]; found {
-					value = cachedValue
-				} else {
-					num := extractInt64(value)
-
-					if num != 0 {
-						tableParts := strings.Split(relatedDataField, ".")
-						tableName := tableParts[0]
-						fieldName := tableParts[1]
-
-						pkField, err := getPrimaryKeyFieldName(db, tableName)
-						if err != nil {
-							log.Printf("WeDyTa: can't determine primary key for table %s: %v", tableName, err)
-						} else {
-							var relatedValue string
-							err = db.Debug().
-								Table(tableName).
-								Select(fieldName).
-								Where(fmt.Sprintf("%s = ?", pkField), value).
-								Row().
-								Scan(&relatedValue)
-
-							if err != nil {
-								//	return "", err
-								relatedDataCache[cacheKey] = relatedValue
-								//log.Printf("Wedyta config problem model=%s relatedData=%s error: %v", modelName, relatedDataField, err)
-								log.Printf("WeDyTa: failed to load related value from %s: %v", tableName, err)
-							} else {
-								value = relatedValue
-								relatedDataCache[cacheKey] = relatedValue
-							}
-						}
-					}
-				}
-			}
-
-			if countConfig, countExists := config.CountRelatedData[field]; countExists {
-				foreignKeyValue, ok := record[countConfig.LocalFieldID]
-				var count int64
-				if ok {
-					if err := db.Debug().Table(countConfig.Table).
-						Where(fmt.Sprintf("%s = ?", countConfig.TargetFieldID), foreignKeyValue).
-						Count(&count).Error; err == nil {
-					}
-				}
-				value = count
-			}
-
-			if linkConfig, linkExists := config.Links[field]; linkExists {
-				link := linkConfig.Template
-				for key, val := range record {
-					placeholder := fmt.Sprintf("$%s$", key)
-					link = strings.ReplaceAll(link, placeholder, fmt.Sprintf("%v", val))
-				}
-				value = fmt.Sprintf("<a href='%s'>%v</a>", link, value)
-			}
-
-			if dateTimeFieldConfig, dateTimeFieldExists := config.DateTimeFields[field]; dateTimeFieldExists {
-				value = extractFormattedTime(value, dateTimeFieldConfig)
-			}
-
+			value, tagAttrs := c.renderRecordValue(db, config, field, record, &cache)
 			htmlTable.WriteString(fmt.Sprintf("\t<td%s>%v</td>\n", tagAttrs, value))
 		}
 		htmlTable.WriteString("</tr>\n")
