b08f17ced891748c433d201e95dd567f05c9f610
Pavel <pavel@p-p.name>
2025-06-23 12:58:41 -0400

    * update doc.go

--- doc.go
+++ doc.go
@@ -1,5 +1,8 @@
 /*
-Package WeDyTa displays GORM database content on the web via Gin without requiring code for table generation.
+Package wedyta displays GORM database content on the web via Gin without requiring code for table generation.
+
+This package is intended to be imported and used by other applications.
+The exported functions, such as New, are used externally and may not be referenced inside this module itself.
 
 See https://github.com/pa-pe/wedyta for more information about wedyta.
 */
