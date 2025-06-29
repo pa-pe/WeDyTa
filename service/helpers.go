060257d485eb1dd8e36f621e348e570f55131008
Pavel <pavel@p-p.name>
2025-06-29 12:36:40 -0400

    + s.checkAccessAndLoadModelConfig()

--- service/helpers.go
+++ service/helpers.go
@@ -2,9 +2,24 @@ package service
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/pa-pe/wedyta/model"
 	"log"
 	"net/http"
 )
+
+func (s *Service) checkAccessAndLoadModelConfig(ctx *gin.Context, modelName string, action string) (bool, *model.ConfigOfModel) {
+	if s.Config.AccessCheckFunc(ctx, modelName, "", action) != true {
+		ctx.String(http.StatusForbidden, "Access Denied")
+		return false, nil
+	}
+
+	mConfig := s.loadModelConfig(ctx, modelName, nil)
+	if mConfig == nil {
+		return false, nil
+	}
+
+	return true, mConfig
+}
 
 func (s *Service) SomethingWentWrong(ctx *gin.Context, logString string) {
 	log.Println("Wedyta: " + logString + " url=" + ctx.Request.URL.String())
